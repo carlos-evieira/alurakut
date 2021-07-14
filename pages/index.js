5e42b027ac7558161626b6f014fadf127d551cd0
kadu <kadu012@gmail.com>
2021-07-13 21:37:12 -0300

    adding user Name

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box >
             <h1 className="title">
-              Bem vindo(a)
+              Bem vindo @{radomUser}
             </h1>
             <OrkutNostalgicIconSet />
           </Box>
